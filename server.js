1f4c7e38cd40721f7def648b81ba1c2bfc696dc9
farhan awan <farhankhanawan@gmail.com>
2022-10-08 23:11:03 +0500

    Express Search API

--- server.js
+++ server.js
@@ -13,48 +13,92 @@ const path = require("path");
 const session = require("express-session")
 app.set('view engine', 'ejs');
 
+var redis = require('redis');
+
+var db = redis.createClient();
+
+// npm install redis
+
+
+// populate search
+
+db.sadd('ferret', 'tobi');
+db.sadd('ferret', 'loki');
+db.sadd('ferret', 'jane');
+db.sadd('cat', 'manny');
+db.sadd('cat', 'luna');
+
+/**
+ * GET search for :query.
+ */
+
+app.get('/search/:query?', function(req, res){
+  var query = req.params.query;
+  db.smembers(query, function(err, vals){
+    if (err) return res.send(500);
+    res.send(vals);
+  });
+});
+
+
+
+
 // Main server app
 
-const main = express();
+// const main = express();
 
-if (!module.parent) main.use(logger('dev'));
+// if (!module.parent) main.use(logger('dev'));
 
-main.get('/', function(req, res){
-  res.send('Hello from main app!');
-});
+// main.get('/', function(req, res){
+//   res.send('Hello from main app!');
+// });
 
-main.get('/:sub', function(req, res){
-  res.send('requested ' + req.params.sub);
-});
+// main.get('/:sub', function(req, res){
+//   res.send('requested ' + req.params.sub);
+// });
 
-// Redirect app
+// // Redirect app
 
-var redirect = express();
+// var redirect = express();
 
-redirect.use(function(req, res){
-  if (!module.parent) console.log(req.vhost);
-  res.redirect('http://example.com:3000/' + req.vhost[0]);
-});
+// redirect.use(function(req, res){
+//   if (!module.parent) console.log(req.vhost);
+//   res.redirect('http://example.com:3000/' + req.vhost[0]);
+// });
 
-// Vhost app
+// // Vhost app
 
-var app = module.exports = express();
+// var app = module.exports = express();
 
-app.use(vhost('*.example.com', redirect)); // Serves all subdomains via Redirect app
-app.use(vhost('example.com', main)); // Serves top level domain via Main server app
+// app.use(vhost('*.example.com', redirect)); // Serves all subdomains via Redirect app
+// app.use(vhost('example.com', main)); // Serves top level domain via Main server app
 
 
 
 // static is virtual path
-app.use('/static', express.static(path.join(__dirname, 'public')))
+// app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')))
+
+/**
+ * GET client javascript. Here we use sendFile()
+ * because serving __dirname with the static() middleware
+ * would also mean serving our server "index.js" and the "search.jade"
+ * template.
+ */
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/client.js', function(req, res){
+  res.sendFile(path.join(__dirname, 'client.js'));
+});
+
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}))
+
 // parses request cookies, populating
 // req.cookies and req.signedCookies
 // when the secret is passed, used
 // for signing the cookies.
+
 app.use(cookieParser('my secret code is here'));
 app.use(session({
     // secret: 'secret', 
@@ -71,26 +115,26 @@ app.use('/route',router);
 // app.get("/", (req, res)=>{
 //     res.render("base", { title : "Home"})
 // })
-app.get('/', function(req, res){
-    if (req.cookies.remember) {
-      res.send('Remembered :). Click to <a href="/forget">forget</a>!.');
-    } else {
-      res.send('<form method="post"><p>Check to <label>'
-        + '<input type="checkbox" name="remember"/> remember me</label> '
-        + '<input type="submit" value="Submit"/>.</p></form>');
-    }
-  });
+// app.get('/', function(req, res){
+//     if (req.cookies.remember) {
+//       res.send('Remembered :). Click to <a href="/forget">forget</a>!.');
+//     } else {
+//       res.send('<form method="post"><p>Check to <label>'
+//         + '<input type="checkbox" name="remember"/> remember me</label> '
+//         + '<input type="submit" value="Submit"/>.</p></form>');
+//     }
+//   });
   
-  app.get('/forget', function(req, res){
-    res.clearCookie('remember');
-    res.redirect('back');
-  });
+//   app.get('/forget', function(req, res){
+//     res.clearCookie('remember');
+//     res.redirect('back');
+//   });
   
-  app.post('/', function(req, res){
-    var minute = 60000;
-    if (req.body.remember) res.cookie('remember', 1, { maxAge: minute });
-    res.redirect('back');
-  });
+//   app.post('/', function(req, res){
+//     var minute = 60000;
+//     if (req.body.remember) res.cookie('remember', 1, { maxAge: minute });
+//     res.redirect('back');
+//   });
 
 
 
