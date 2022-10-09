b709647ff434a06eb2809d1a0e29bdae9da25c2a
farhan awan <farhankhanawan@gmail.com>
2022-10-09 22:43:59 +0500

    Files Download Example

--- server.js
+++ server.js
@@ -13,6 +13,27 @@ const path = require("path");
 const session = require("express-session")
 app.set('view engine', 'ejs');
 
+// path to where the files are stored on disk
+var FILES_DIR = path.join(__dirname, 'files')
+
+app.get('/', function(req, res){
+  res.send('<ul>' +
+        '<li>Download <a href="/files/Hello.txt">Hello.txt</a>.</li>' +
+          '</ul>')
+});
+
+// /files/* is accessed via req.params[0]
+// but here we name it :file
+app.get('/files/:file(*)', function(req, res, next){
+  res.download(req.params.file, { root: FILES_DIR }, function (err) {
+    if (!err) return; // file sent
+    if (err.status !== 404) return next(err); // non-404 error
+    // file for download not found
+    res.statusCode = 404;
+    res.send('Cant find that file, sorry!');
+  });
+});
+
 var redis = require('redis');
 
 var db = redis.createClient();
