1150baee799ac490ab3506dca6ed75fbde8b1e97
farhan awan <farhankhanawan@gmail.com>
2022-10-13 22:54:52 +0500

    Some correction

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const bodyparser = require("body-parser");
 const router = require('./router');
 var vhost = require('vhost');
-const {v4:uuidv4} = require("uuid");
+// const {v4:uuidv4} = require("uuid");
 var cookieParser = require('cookie-parser');
 var multiparty = require('multiparty');
 var format = require('util').format;
@@ -190,34 +190,34 @@ app.use('/route',router);
 
 // Fake controller.
 
-// var User = {
-//   index: function(req, res){
-//     res.send(users);
-//   },
-//   show: function(req, res){
-//     res.send(users[req.params.id] || { error: 'Cannot find user' });
-//   },
-//   destroy: function(req, res, id){
-//     var destroyed = id in users;
-//     delete users[id];
-//     res.send(destroyed ? 'destroyed' : 'Cannot find user');
-//   },
-//   range: function(req, res, a, b, format){
-//     var range = users.slice(a, b + 1);
-//     switch (format) {
-//       case 'json':
-//         res.send(range);
-//         break;
-//       case 'html':
-//       default:
-//         var html = '<ul>' + range.map(function(user){
-//           return '<li>' + user.name + '</li>';
-//         }).join('\n') + '</ul>';
-//         res.send(html);
-//         break;
-//     }
-//   }
-// };
+var User = {
+  index: function(req, res){
+    res.send(users);
+  },
+  show: function(req, res){
+    res.send(users[req.params.id] || { error: 'Cannot find user' });
+  },
+  destroy: function(req, res, id){
+    var destroyed = id in users;
+    delete users[id];
+    res.send(destroyed ? 'destroyed' : 'Cannot find user');
+  },
+  range: function(req, res, a, b, format){
+    var range = users.slice(a, b + 1);
+    switch (format) {
+      case 'json':
+        res.send(range);
+        break;
+      case 'html':
+      default:
+        var html = '<ul>' + range.map(function(user){
+          return '<li>' + user.name + '</li>';
+        }).join('\n') + '</ul>';
+        res.send(html);
+        break;
+    }
+  }
+};
 
 // curl http://localhost:3000/users     -- responds with all users
 // curl http://localhost:3000/users/1   -- responds with user 1
